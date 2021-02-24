c15268becb54db3c32b0dd239918a7dd7034ec5a
ranog <jprnogueira@yahoo.com.br>
2021-02-24 17:05:19 -0300

    FUNÇÕES (pg 95-96).

---
+++ helloFunc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+def hello():
+    """
+        Imprimi algumas saudações.
+    """
+    print("Howdy!")
+    print("Howdy!!!")
+    print("Hello there.")
+
+
+hello()
+hello()
+hello()
