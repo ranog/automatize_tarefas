94e2f5763a6427bf1c0e7aff1d34a18e1f3cd84e
ranog <jprnogueira@yahoo.com.br>
2021-04-20 12:57:55 -0300

    Sequência de Collatz (pg 112).

--- sequenciaCollatz.py
+++ sequenciaCollatz.py
@@ -51,11 +51,18 @@ inteiro deve ser fornecido. """
 def collatz(number):
     """ Conjectura de Collatz. """
     if number % 2 == 0:
-        print(number // 2)
+        return number // 2
     elif number % 2 == 1:
-        print(3 * number + 1)
+        return 3 * number + 1
 
 
-number = int(input("Enter number: "))
+number = int(input("Enter number: \n"))
 
-collatz(number)
+while True:
+    resultado = collatz(number)
+    print(resultado)
+    
+    if resultado == 1:
+        break
+    else:
+        number = int(input())
