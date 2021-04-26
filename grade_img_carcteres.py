54a17eaa5b7c5ae6a9cec9ab5188abd3d746c48a
ranog <jprnogueira@yahoo.com.br>
2021-04-26 13:22:22 -0300

    Comparação entre dicionários e listas (pg 144-146).

--- grade_img_carcteres.py
+++ grade_img_carcteres.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+""" Podemos pensar em grid[x][y] como sendo o caractere nas coordenadas
+x e y de uma “imagem” desenhada com caracteres textuais. A origem (0, 0)
+estará no canto superior esquerdo, as coordenadas x aumentam para a
+direita e as coordenadas y aumentam para baixo. """
+
 grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
