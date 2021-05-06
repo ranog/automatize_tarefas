ed1e956780809cb8bf0b0bc41d3361eb26c055bf
ranog <jprnogueira@yahoo.com.br>
2021-05-06 12:35:21 -0300

    Passo 1: Design do programa e estruturas de dados (pg 178).

---
+++ pw.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+""" pw.py - Um programa para repositório de senhas que não é seguro. """
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+        'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+        'luggage': '12345'}
