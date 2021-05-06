835497d8374bb98e2cd23d91f7c2fcb22e08265b
ranog <jprnogueira@yahoo.com.br>
2021-05-06 12:47:15 -0300

    Passo 2: Tratar argumentos da linha de comando (pg 178-179).

--- pw.py
+++ pw.py
@@ -2,6 +2,17 @@
 
 """ pw.py - Um programa para repositório de senhas que não é seguro. """
 
+import sys
+
 PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
         'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
         'luggage': '12345'}
+
+if len(sys.argv) < 2:
+    print("Usage: python pw.py [account] - copy account password")
+    sys.exit()
+
+account = sys.argv[1]   # O primeiro argumento da linha de comando é o
+                        # nome da conta.
+
+
