4b2d237a1b71d6f16dde409e77f7059a41985085
ranog <jprnogueira@yahoo.com.br>
2021-05-07 11:35:58 -0300

    Passo 3: Juntar as linhas modificadas (pg 183).

--- bulletPointAdder.py
+++ bulletPointAdder.py
@@ -15,4 +15,8 @@ for i in range(len(lines)):
     # Acrescenta um asterisco em cada string da lista "lines".
     lines[i] = '* ' + lines[i]
 
+# Passamos "lines" para o método join() a fim de obter uma única string
+# resultante da junção das strings da lista. 
+text = '\n'.join(lines)
+
 pyperclip.copy(text)
