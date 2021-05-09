24c36b2b3a90eb2b3fa3154922c995297c007440
ranog <jprnogueira@yahoo.com.br>
2021-05-09 13:50:06 -0300

    Projeto prático - Exibição de tabela (pg 184-185).

--- exibicao_de_tabela.py
+++ exibicao_de_tabela.py
@@ -29,10 +29,43 @@ por diante. Você poderá então identificar o maior valor na lista
 colWidths e descobrir a largura na forma de um inteiro a ser passada
 para o método de string rjust(). """
 
+
 def printTable(tableData):
     """ Exiba em uma tabela bem organizada, com cada coluna justificada
     à direita. """
+
+    # Criará uma lista contendo o mesmo número de valores 0 que o número
+    # de listas internas em tableData.
     colWidths = [0] * len(tableData)
+
+    # O laço while a seguir, armazena em colWidths[0] a largura da string
+    # mais longa de tableData[0], em colWidths[1] a largura da string
+    # mais longa de tableData[1] e assim por diante... 
+    i = 0
+    while i < len(tableData):
+        st_length = []
+        for st in tableData[i]:
+            st_length.append(len(st))
+        st_length.sort()
+        colWidths[i] = st_length[-1]
+        i += 1
+
+    # Organiza a lista colWidths[] e atribui a maior largura na forma de
+    # um inteiro na variavel width que é passada para o métoro de string
+    # rjust().
+    colWidths.sort()
+    width = colWidths[-1]
+
+    # O laço a seguir é responsável por exibir a tabela de forma
+    # organizada, com cada coluna justificada à direita.
+    j = 0
+    while j < len(tableData):
+        for item in tableData[j]:
+            print(item.rjust(width), end=' ')
+
+        print()
+        j += 1
+
 
 tableData = [['apples', 'oranges', 'cherries', 'banana'],
         ['Alice', 'Bob', 'Carol', 'David'],
