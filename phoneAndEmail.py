97dbf00a8a09ee77fe274450b768937f52c731b4
ranog <jprnogueira@yahoo.com.br>
2021-05-23 00:18:11 -0300

    Passo 1: Criar uma regex para números de telefone (pg 209-210).

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -1,3 +1,18 @@
 #! python3
 # phoneAndEmail.py – Encontra números de telefone e endereços de email
 # no clipboard.
+
+import pyperclip, re
+
+phoneRegex = re.compile(r'''(
+        (\d{3}|\(\d{3}\))?  # código de área
+        (\s|-|\.)?          # separador
+        (\d{3})             # primeiros 3 dígitos
+        (\s|-|\.)?          # separador
+        (\d{4})             # últimos 4 dígitos
+        (\s*(exp|x|exp.)\s*(\d{2,5}))?  # extensão
+        )'''.re.VERBOSE)
+
+# TODO: Cria a regex para email.
+# TODO: Encontra correspondências no texto do clipboard.
+# TODO: Copia os resultados para o clipboard.
